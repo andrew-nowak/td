cda1a9059a93bc77c007a035eaa9e216f10c933d
Andrew Nowak <andrew@nowak.me.uk>
2023-02-19 15:35:27 +0000

    add usage string

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, process};
 
 use time::format_description::FormatItem;
 use time::{macros::format_description, OffsetDateTime};
@@ -21,9 +21,29 @@ fn display(date: &OffsetDateTime) {
 	println!("{}", date.format(&FORM).unwrap());
 }
 
+fn display_usage(arg0: &str) {
+	println!("Usage: {} [options] [datetime]", arg0);
+	println!("");
+	println!("  datetime: A string representing a datetime to be formatted");
+	println!("  options:");
+	println!("    -h, --help, --usage: Print this usage information and exit");
+	process::exit(1);
+}
+
 fn main() {
+	let mut all_args = env::args();
+	let arg0 = all_args.next().unwrap();
 	let (params, time_bits): (Vec<String>, Vec<String>) =
-		env::args().skip(1).partition(|arg| arg.starts_with("-"));
+		all_args.partition(|arg| arg.starts_with("-"));
+
+	if params.contains(&"-h".to_string())
+		|| params.contains(&"--help".to_string())
+		|| params.contains(&"--usage".to_string())
+		|| (time_bits.contains(&"help".to_string()))
+	{
+		display_usage(&arg0);
+		return;
+	}
 
 	if time_bits.len() == 1 {
 		let x = as_unix_timestamp(&time_bits[0]);
